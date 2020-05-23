306ff8b0521f95b083c11041b48045e38ae5fcf6
Ali <eng.aliarous@gmail.com>
2020-05-23 16:55:38 +0200

    added: buy new orders feature

--- app.py
+++ app.py
@@ -39,10 +39,10 @@ handler = watchtower.CloudWatchLogHandler()
 app.logger.addHandler(handler)
 logging.getLogger("werkzeug").addHandler(handler)
 
-@app.route('/log')
-def log():
-    app.logger.info("Amazing! :)")
-    return 'logged! :)'
+# @app.route('/log')
+# def log():
+#     app.logger.info("Amazing! :)")
+#     return 'logged! :)'
 
 @app.route('/')
 def hello_world():
@@ -59,15 +59,29 @@ def test_db():
     users.insert_one({'_id':get_next_sequence_value(counters, 'user_id') , 'name': 'Julio', 'age':'26', 'address': 'Peru'})
     return dumps(users.find())
 
+@app.route('/buy-orders', methods=["GET", "POST"])
+@aws_auth.authentication_required
+def buy_orders():
+    data = request.json
+    u = users.find_and_modify(query={"email": aws_auth.claims['email']},
+                              update={"$inc": {"quota": data['num']}})
+    if u is None:
+        return jsonify('ERROR: you are not authorized to run this command!'), 403
+    else:
+        return jsonify('Congratulations! You have loaded your account with '+data['num']+' new orders!')
+
 @app.route('/get-dashboard', methods=["GET", "POST"])
 @aws_auth.authentication_required
 def get_dashboard():
     #
+    u = users.find_one({'email': aws_auth.claims['email']})
+    quota = -1 if u is None else u['quota']
     enqueued = sims.find({'user_id': aws_auth.claims['email'], 'status':'ENQUEUED'}, ['order_time'])
-    ready = sims.find({'status':'READY'}, ['order_time'])
+    ready = sims.find({'user_id': aws_auth.claims['email'], 'status':'READY'}, ['order_time'])
     enqueued_list =  [] if enqueued is None else list(enqueued)
     ready_list = [] if ready is None else list(ready)
     response = {
+        'quota': quota,
         'enqueued_count': len(enqueued_list),
         'enqueued_list': enqueued_list,
         'ready_count': len(ready_list),
@@ -84,10 +98,10 @@ def get_simulation():
     s = sims.find_one({'_id': int(sim_id), 'user_id': aws_auth.claims['email']})
     # check if the simulation belongs to the current user
     if s is None:
-        app.logger.info("SIMULATION_VIEW: by user " + aws_auth.claims['email'])
-        return jsonify('YOU DO NOT HAVE PERMISSION TO ACCESS THIS SIMULATION!')
-    else:
         app.logger.warning("UNAUTHORIZED_SIMULATION_VIEW: by user " + aws_auth.claims['email'])
+        return jsonify('YOU DO NOT HAVE PERMISSION TO ACCESS THIS SIMULATION!'), 403
+    else:
+        app.logger.info("SIMULATION_VIEW: by user " + aws_auth.claims['email'])
         return jsonify(s)
 
 @app.route('/register-user', methods=["POST"])
@@ -99,7 +113,7 @@ def register():
         return jsonify('This username is already registered!')
     su=users.insert_one({
         '_id': get_next_sequence_value(counters, 'user_id'),
-        'first_name': data['firstq_name'],
+        'first_name': data['first_name'],
         'last_name': data['last_name'],
         'email': data['email'],
         'quota': 20,
@@ -119,35 +133,40 @@ def login():
 @aws_auth.authentication_required
 def enqueue():
     data = request.json
-    s=sims.insert_one({
-        '_id':get_next_sequence_value(counters, 'simulation_id'),
-        'order_time': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-        'parameters': data,
-        'status': 'ENQUEUED',
-        'user_id': aws_auth.claims['email']
-    })
-    # return jsonify(data)
-    # Send message to SQS queue
-    if s is not None:
-        response = queue.send_message(
-            # QueueUrl=queue_url,
-            DelaySeconds=10,
-            MessageBody=dumps(data),
-            MessageAttributes={
-                'user_id': {
-                    'DataType': 'String',
-                    'StringValue': aws_auth.claims['email']
-                },
-                'simulation_id': {
-                    'DataType': 'String',
-                    'StringValue': str(s.inserted_id)
-                },
-            },
-        )
-        app.logger.info("NEW_SIMULATION_ORDER: by user " + aws_auth.claims['email'])
-        return jsonify(response['MessageId'])
+    u = users.find_and_modify(query={"email": aws_auth.claims['email'], "quota":{"$gt":0}}, update={"$inc":{"quota": -1}})
+    if u is None:
+        app.logger.warning("ORDER_WITH_INSUFFICIENT_QUOTA: by user " + aws_auth.claims['email'])
+        return jsonify("ERROR: you have no orders left! Buy new orders to run this command.."), 403
     else:
-        return jsonify('ERROR: Could not store the simulation order in the database')
+        s=sims.insert_one({
+            '_id':get_next_sequence_value(counters, 'simulation_id'),
+            'order_time': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            'parameters': data,
+            'status': 'ENQUEUED',
+            'user_id': aws_auth.claims['email']
+        })
+        # return jsonify(data)
+        # Send message to SQS queue
+        if s is not None:
+            response = queue.send_message(
+                # QueueUrl=queue_url,
+                DelaySeconds=10,
+                MessageBody=dumps(data),
+                MessageAttributes={
+                    'user_id': {
+                        'DataType': 'String',
+                        'StringValue': aws_auth.claims['email']
+                    },
+                    'simulation_id': {
+                        'DataType': 'String',
+                        'StringValue': str(s.inserted_id)
+                    },
+                },
+            )
+            app.logger.info("NEW_SIMULATION_ORDER: by user " + aws_auth.claims['email'])
+            return jsonify(response['MessageId'])
+        else:
+            return jsonify('ERROR: Could not store the simulation order in the database'), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
